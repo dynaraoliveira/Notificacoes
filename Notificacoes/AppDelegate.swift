66d151c33d63979e7918dff669dc18ffa77200ab
Usuário Convidado <Guest@PA06MICRO07.local>
2019-03-18 21:46:33 -0300

    aula notificacao

--- Notificacoes/AppDelegate.swift
+++ Notificacoes/AppDelegate.swift
@@ -7,15 +7,46 @@
 //
 
 import UIKit
+import UserNotifications
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
-
+    var center = UNUserNotificationCenter.current()
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        center.delegate = self
+        center.getNotificationSettings { (settings) in
+            switch settings.authorizationStatus {
+            case .authorized:
+                print("Usuário autorizou")
+            case .denied:
+                print("Usuário não autorizou")
+            case .notDetermined:
+                let options: UNAuthorizationOptions = [.alert, .sound, .carPlay, .badge]
+                self.center.requestAuthorization(options: options, completionHandler: { (authorized, error) in
+                    if error != nil {
+                        print("Deu merda:", error!)
+                    } else {
+                        print("Autorizado?", authorized)
+                    }
+                })
+            case .provisional:
+                print("Ver depois!! é muito novo e o tio Eric nunca usou")
+            }
+        }
+        
+        let confirmAction = UNNotificationAction(identifier: "Confirm", title: "Confirmar 👍🏻", options: [.foreground])
+        let cancelAction = UNNotificationAction(identifier: "Cancel", title: "Cancelar ☠︎", options: [])
+        let category = UNNotificationCategory(identifier: "Lembrete", actions: [confirmAction, cancelAction],
+                                              intentIdentifiers: [], hiddenPreviewsBodyPlaceholder: "",
+                                              options: [.customDismissAction])
+        
+        center.setNotificationCategories([category])
+        
         return true
     }
 
@@ -34,13 +65,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
 
     func applicationDidBecomeActive(_ application: UIApplication) {
-        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+        UIApplication.shared.applicationIconBadgeNumber = 0
     }
 
     func applicationWillTerminate(_ application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
-
-
 }
 
+extension AppDelegate: UNUserNotificationCenterDelegate { }
--- Notificacoes/ViewController.swift
+++ Notificacoes/ViewController.swift
@@ -7,14 +7,41 @@
 //
 
 import UIKit
+import UserNotifications
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tfCourse: UITextField!
+    @IBOutlet weak var tfSubject: UITextField!
+    @IBOutlet weak var dpDate: UIDatePicker!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    @IBAction func scheduleNotification(_ sender: UIButton) {
+        
+        let content = UNMutableNotificationContent()
+        content.title = "Lembrete"
+        content.subtitle = "Curso: \(tfCourse.text!)"
+        content.body = "Está na hora de estudar \(tfSubject.text!)"
+        content.badge = 1
+        content.categoryIdentifier = "Lembrete"
+        
+        //let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 10, repeats: false)
+        
+        let date = dpDate.date
+        let dateComponents = Calendar.current.dateComponents([.year, .month, .day, .hour, .minute], from: date)
+        
+        let trigger = UNCalendarNotificationTrigger(dateMatching: dateComponents, repeats: false)
+        
+        let request = UNNotificationRequest(identifier: "lembrete", content: content, trigger: trigger)
+        UNUserNotificationCenter.current().add(request) { (error) in
+            if let error = error {
+                print("Erro na notificação:", error)
+            }
+        }
+    }
+    
 }
 
